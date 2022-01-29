0045159d5a543f916641de4c3a5674bfac01ee39
Sam Hicks <samueljacksonhicks@gmail.com>
2022-01-29 00:34:35 -0600

    piece and board class are here!

--- chess.py
+++ chess.py
@@ -1,15 +1,44 @@
-BOARDSIZE = 5
-board = []
+WHITE = True
+BLACK = False
+
+class Board:
+    def __init__(self, boardSize):
+        self.state = []
+        for i in range(boardSize):
+            self.state.append('0 0 0 0 0'.split())
+
+    def display(self):
+        for row in self.state:
+            print(' '.join(row))
+
+
+    def updatePiece(self, piece):
+        self.state[piece.pos[0]][piece.pos[1]] = piece.symbol
+        
 
 class Piece:
-    def __init__(self):
-        self.symbol = "*"
+    def __init__(self, pos):
+        self.pos = pos
+        board.updatePiece(self)
 
+    def move(self, pos):
+        self.pos = pos
+        board.updatePiece(self)
+
+
+class O(Piece):
+    def __init__(self, pos, color):
+        self.symbol = 'x' if(color) else 'y'
+        super().__init__(pos)
+
+board = Board(5)
 
 def main():
-    x = Piece()
+    o = O([1, 1], WHITE)
 
 
+
+    board.display()
 
 if __name__ == "__main__":
     main()
