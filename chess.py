169b77ce61e882229ce8dca1b6579f3dd4c2588d
Sam Hicks <samueljacksonhicks@gmail.com>
2022-02-14 23:07:19 -0600

    pawns can now find their moves

--- chess.py
+++ chess.py
@@ -1,24 +1,31 @@
 WHITE = True
 BLACK = False
 
+X = 1
+Y = 0
+
+BOARDSIZE = 8
+
+
 class Board:
     def __init__(self, boardSize):
         self.state = []
         for i in range(boardSize):
-            self.state.append('0 0 0 0 0'.split())
+            self.state.append('0 0 0 0 0 0 0 0'.split())
 
     def display(self):
         for row in self.state:
             print(' '.join(row))
 
-
     def updatePiece(self, piece):
-        self.state[piece.pos[0]][piece.pos[1]] = piece.symbol
+        self.state[piece.pos[Y]][piece.pos[X]] = piece.symbol
         
-
 class Piece:
-    def __init__(self, pos):
+    def __init__(self, pos, color):
+        # self.pos = [Y, X]
         self.pos = pos
+        self.color = color
+        self.possibleMoves = []
         board.updatePiece(self)
 
     def move(self, pos):
@@ -26,17 +33,30 @@ class Piece:
         board.updatePiece(self)
 
 
-class O(Piece):
+class Pawn(Piece):
     def __init__(self, pos, color):
         self.symbol = 'x' if(color) else 'y'
-        super().__init__(pos)
+        super().__init__(pos, color)
+        self.findMoves()
 
-board = Board(5)
+    def findMoves(self):
+        if self.color:
+            self.possibleMoves.append( [self.pos[Y] - 1, self.pos[X]] )
+            if self.pos[Y] == BOARDSIZE - 2:
+                self.possibleMoves.append( [self.pos[Y] - 2, self.pos[X]] )
+        else:
+            self.possibleMoves.append( [self.pos[Y] + 1, self.pos[X]] )
+            if self.pos[Y] == 1:
+                self.possibleMoves.append( [self.pos[Y] + 2, self.pos[X]] )
+
+
+board = Board(BOARDSIZE)
 
 def main():
-    o = O([1, 1], WHITE)
-
-
+    p1 = Pawn([BOARDSIZE - 2, 0], WHITE)
+    p2 = Pawn([1, 0], BLACK)
+    print(p1.possibleMoves)
+    print(p2.possibleMoves)
 
     board.display()
 
